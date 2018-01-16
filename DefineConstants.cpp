9080da6d0bbf2a634c4629e03bc58ad3c4c6e0e4
Letty <lettycheungzl@gmail.com>
2018-01-16 21:39:29 +0800

    cannot assign to variable 'weightGoal' with const-qualified type 'const int'

---
+++ DefineConstants.cpp
@@ -0,0 +1,13 @@
+/* GOAL: use constant variable */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int weightGoal = 100;
+    cout << "WeightGoal = " << weightGoal << endl;
+    // weightGoal = 200;
+    // cout << "WeightGoal = " << weightGoal << endl;
+    return 0;
+}
