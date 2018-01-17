a96dc6d5892c206776e158717a2ffbd92eec28be
Letty <lettycheungzl@gmail.com>
2018-01-17 15:51:23 +0800

    practice using cout and setw() or \t to format output to console.

--- FormattingOutput.cpp
+++ FormattingOutput.cpp
@@ -20,10 +20,17 @@ int main() {
     float bbb = bb + 9;
     double ccc = cc + 9;
 
+    std::cout << "Print with setw():" << std::endl;
     std::cout << "Ints" << std::setw(8) << "Floats" << std::setw(10) << "Doubles" << std::endl;
     std::cout << a << std::setw(10) << b << std::setw(10) << c << std::endl;
     std::cout << aa << std::setw(10) << bb << std::setw(10) << cc << std::endl;
     std::cout << aaa << std::setw(10) << bbb << std::setw(10) << ccc << std::endl;
-    
+
+    std::cout << "Print with tab:" << std::endl;
+    std::cout << "Ints" << "\t" << "Floats" << "\t" << "Doubles" << std::endl;
+    std::cout << a << "\t" << b << "\t" << c << std::endl;
+    std::cout << aa << "\t" << bb << "\t" << cc << std::endl;
+    std::cout << aaa << "\t" << bbb << "\t" << ccc << std::endl;
+
     return 0;
 }
