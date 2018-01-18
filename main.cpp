19e76666266cfda0826977e9ed93b7beb056f543
Letty <lettycheungzl@gmail.com>
2018-01-18 11:30:31 +0800

    Practice creating header files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* GOAL: practice using header files.
+** Put in the header file every thing that is related to
+** "How to do a task".
+** Put the "What to do" in the main program.
+*/
+
+#include "main.hpp"
+
+int main() {
+    cout << "Hello, I use header files!";
+    return 0;
+}
---
+++ main.hpp
@@ -0,0 +1,10 @@
+/* This is a header file.
+** Put in a header file every thing that is related to
+** "How to do a task".
+** Put the "What to do" in the main program.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
