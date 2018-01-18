99dc55b7f17c9068220030e54fe5b2d95aec97cd
Letty <lettycheungzl@gmail.com>
2018-01-18 16:02:09 +0800

    The example for using std::cin and std::getline.

---
+++ StringInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string userName;
+    std::cout << "Tell me your nickname?: ";
+    std::getline(std::cin, userName);
+    std::cout << "Hello, " << userName << std::endl;
+    return 0;
+}
