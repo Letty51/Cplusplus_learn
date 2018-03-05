7b43d7b8fb27e2d10e642ed7135e545231cab4a4
Letty51 <lettycheungzl@gmail.com>
2018-03-05 20:58:47 +0800

    Pointers

---
+++ Pointers.cpp
@@ -0,0 +1,13 @@
+/* GOAL: Examine pointers! */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 54;
+    cout << "a = " << a << "\n";
+    cout << "address of a is at &a = " << &a << "\n";
+    return 0;
+}
