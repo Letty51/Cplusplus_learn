0d6cf705539cdf0b649754cb5a413b00579f763e
Letty51 <lettycheungzl@gmail.com>
2018-03-05 20:54:46 +0800

    while Loops

---
+++ whileLoops.cpp
@@ -0,0 +1,26 @@
+/* GOAL: understand the syntax of while loops. */
+
+#include <iostream>
+
+int main()
+{
+    int entry = 0;
+
+    // with this while loop the condition is true
+    // so the statements are executed
+    while(entry <= 5)
+    {
+        std::cout << "incrementing entry = " << entry << "\n";
+        entry++;
+    }
+
+    // with this while loop the condition is false
+    // so the statements are not executed
+    while(entry < 5)
+    {
+        std::cout << "decrememting entry = " << entry << "\n";
+        entry--;
+    }
+
+    return 0;
+}
