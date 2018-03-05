24ef253b4b1878277465bb92e9500d270c171fe3
Letty51 <lettycheungzl@gmail.com>
2018-03-05 20:55:04 +0800

    while loop

---
+++ whileLoop.cpp
@@ -0,0 +1,19 @@
+/* GOAL: In the programming quiz, use a while loop to prompt the user to guess a target number.
+** Tell the user if the guess is too high or too low.
+** The user enters 'q' or guesses the target number to end the program.
+*/
+
+#include <iostream>
+#include <sstream>
+//#include <string>
+using namespace std;
+int main()
+{
+    cout << "123";
+    int target = 55;
+    //string userString;
+    int guess = -1;
+    cout << "456";
+    
+    return 0;
+}
